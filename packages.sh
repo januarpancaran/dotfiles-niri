921f70a70d7857600e63c3d17f756d6e02380dfd
januarpancaran <januar352@gmail.com>
2025-07-26 16:10:23 +0700

    change: changing file indentation

--- packages.sh
+++ packages.sh
@@ -1,104 +1,107 @@
 #!/bin/bash
 
 APPS=(
-  bibata-cursor-theme-bin
-  blueberry
-  discord
-  dracula-gtk-theme
-  dracula-icons-theme
-  ghostty
-  gnome-control-center
-  google-chrome
-  mpv
-  nautilus
-  neovim
-  networkmanager
-  niri
-  obs-studio
-  papirus-icon-theme
-  spotify-launcher
-  telegram-desktop
-  visual-studio-code-bin
+    bibata-cursor-theme-bin
+    blueberry
+    discord
+    dracula-gtk-theme
+    dracula-icons-theme
+    ghostty
+    gnome-control-center
+    google-chrome
+    mpv
+    nautilus
+    neovim
+    networkmanager
+    niri
+    obs-studio
+    papirus-icon-theme
+    spotify-launcher
+    telegram-desktop
+    visual-studio-code-bin
 )
 
 UTILS=(
-  # Sound
-  alsa-firmware
-  alsa-utils
-  pipewire
-  pipewire-alsa
-  pipewire-pulse
-  wireplumber
+    # Sound
+    alsa-firmware
+    alsa-utils
+    pipewire
+    pipewire-alsa
+    pipewire-pulse
+    wireplumber
 
-  # Backlight
-  acpi
-  brightnessctl
+    # Backlight
+    acpi
+    brightnessctl
 
-  # Bluetooth
-  bluez
-  bluez-utils
+    # Bluetooth
+    bluez
+    bluez-utils
 
-  # Storage
-  gvfs
+    # Storage
+    gvfs
 
-  # AUTH
-  polkit-gnome
-  xorg-xhost
+    # AUTH
+    polkit-gnome
+    xorg-xhost
 
-  # Timeshift
-  timeshift-autosnap
+    # Timeshift
+    timeshift-autosnap
 
-  # Others
-  bat
-  curl
-  fastfetch
-  fuzzel
-  fzf
-  git
-  htop
-  nodejs
-  npm
-  ntfs-3g
-  openssh
-  os-prober
-  playerctl
-  ripgrep
-  starship
-  swaybg
-  tk
-  tlp
-  tmux
-  trash-cli
-  unrar
-  unzip
-  wget
-  wl-clipboard
-  xdg-desktop-portal-gnome
-  xdg-desktop-portal-gtk
-  xwayland-satellite
-  xwaylandvideobridge
-  yazi
-  zip
-  zoxide
-  zsh
-  pyenv
+    # Others
+    bat
+    curl
+    fastfetch
+    fuzzel
+    fzf
+    git
+    htop
+    hypridle
+    hyprlock
+    nodejs
+    npm
+    ntfs-3g
+    openssh
+    os-prober
+    playerctl
+    pyenv
+    ripgrep
+    starship
+    swaybg
+    tk
+    tlp
+    tmux
+    trash-cli
+    unrar
+    unzip
+    wget
+    wl-clipboard
+    xdg-desktop-portal-gnome
+    xdg-desktop-portal-gtk
+    xwayland-satellite
+    xwaylandvideobridge
+    yazi
+    zip
+    zoxide
+    zsh
 )
 
 FONTS=(
-  noto-fonts-cjk
-  noto-fonts-emoji
-  otf-font-awesome
-  ttf-jetbrains-mono-nerd
-  ttf-space-mono-nerd
-  ttf-ms-win11-auto
+    noto-fonts-cjk
+    noto-fonts-emoji
+    otf-font-awesome
+    ttf-jetbrains-mono-nerd
+    ttf-space-mono-nerd
+    ttf-ms-win11-auto
+    ttf-material-symbols-variable-git
 )
 
 SERVICES=(
-  # System
-  NetworkManager
-  bluetooth
-  tlp
+    # System
+    NetworkManager
+    bluetooth
+    tlp
 
-  # User
-  pipewire
+    # User
+    pipewire
 )
