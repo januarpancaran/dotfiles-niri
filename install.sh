04941c28e15b28a6d919994bab792fcb617e8d92
januarpancaran <januar352@gmail.com>
2025-07-26 15:16:09 +0700

    change: changing login script

--- install.sh
+++ install.sh
@@ -1,72 +1,72 @@
 #!/bin/bash
 
 command_exists() {
-  command -v "$1" &>/dev/null
+	command -v "$1" &>/dev/null
 }
 
 # Checking sudo command
 SUDO_CMD=""
 
 if command_exists sudo; then
-  SUDO_CMD="sudo"
+	SUDO_CMD="sudo"
 elif command_exists doas; then
-  SUDO_CMD="doas"
+	SUDO_CMD="doas"
 else
-  echo "No sudo command detected!"
-  exit 1
+	echo "No sudo command detected!"
+	exit 1
 fi
 
 # Checking AUR Helper
 AUR_HELPER=""
 
 install_aur_helper() {
-  HELPER="$1"
-  TMP_HELPER_DIR=$(mktemp -d -t aur-helper-XXXXXX)
+	HELPER="$1"
+	TMP_HELPER_DIR=$(mktemp -d -t aur-helper-XXXXXX)
 
-  "${SUDO_CMD}" pacman -Syu --noconfirm git
-  git clone https://aur.archlinux.org/"${HELPER}".git "${TMP_HELPER_DIR}"
-  (cd "${TMP_HELPER_DIR}" && makepkg -si --noconfirm)
-  rm -rf "${TMP_HELPER_DIR}"
+	"${SUDO_CMD}" pacman -Syu --noconfirm git
+	git clone https://aur.archlinux.org/"${HELPER}".git "${TMP_HELPER_DIR}"
+	(cd "${TMP_HELPER_DIR}" && makepkg -si --noconfirm)
+	rm -rf "${TMP_HELPER_DIR}"
 
-  check_aur_helper
+	check_aur_helper
 }
 
 check_aur_helper() {
-  if command_exists yay; then
-    AUR_HELPER="yay"
-  elif command_exists paru; then
-    AUR_HELPER="paru"
-  else
-    echo "No AUR Helper detected!"
-    echo "Choose AUR Helper to install: [1 or 2]: "
-    echo "1) yay"
-    echo "2) paru"
-    read -p "Enter your choice: [1 or 2]: " aur_choice
+	if command_exists yay; then
+		AUR_HELPER="yay"
+	elif command_exists paru; then
+		AUR_HELPER="paru"
+	else
+		echo "No AUR Helper detected!"
+		echo "Choose AUR Helper to install: [1 or 2]: "
+		echo "1) yay"
+		echo "2) paru"
+		read -p "Enter your choice: [1 or 2]: " aur_choice
 
-    case "$aur_choice" in
-    1)
-      echo "Installing yay..."
-      install_aur_helper yay
-      AUR_HELPER="yay"
-      ;;
+		case "$aur_choice" in
+		1)
+			echo "Installing yay..."
+			install_aur_helper yay
+			AUR_HELPER="yay"
+			;;
 
-    2)
-      echo "Installing paru..."
-      install_aur_helper paru
-      AUR_HELPER="paru"
-      ;;
-    *)
-      echo "Invalid choice."
-      exit 1
-      ;;
-    esac
-  fi
+		2)
+			echo "Installing paru..."
+			install_aur_helper paru
+			AUR_HELPER="paru"
+			;;
+		*)
+			echo "Invalid choice."
+			exit 1
+			;;
+		esac
+	fi
 }
 
 check_aur_helper
 
 install_pkg() {
-  "${AUR_HELPER}" -Syu --noconfirm "$@"
+	"${AUR_HELPER}" -Syu --noconfirm "$@"
 }
 
 source packages.sh
@@ -114,11 +114,11 @@ EOF
 
 echo "Enabling services..."
 for service in "${SERVICES[@]}"; do
-  if [ "$service" = "pipewire" ]; then
-    systemctl enable --user --now "$service"
-  else
-    "${SUDO_CMD}" systemctl enable --now "$service"
-  fi
+	if [ "$service" = "pipewire" ]; then
+		systemctl enable --user --now "$service"
+	else
+		"${SUDO_CMD}" systemctl enable --now "$service"
+	fi
 done
 
 echo "Copying config files..."
@@ -126,22 +126,22 @@ CONF_SRC="./dotconfig"
 CONF_DIR=""
 
 if [ -d "$XDG_CONFIG_HOME" ]; then
-  CONF_DIR="$XDG_CONFIG_HOME"
+	CONF_DIR="$XDG_CONFIG_HOME"
 else
-  if [ ! -d "$HOME/.config" ]; then
-    mkdir -p "$HOME/.config"
-  fi
+	if [ ! -d "$HOME/.config" ]; then
+		mkdir -p "$HOME/.config"
+	fi
 
-  CONF_DIR="$HOME/.config"
+	CONF_DIR="$HOME/.config"
 fi
 
 for item in "${CONF_SRC}"/*; do
-  name=$(basename "$item")
-  if [ -d "${CONF_DIR}/${name}" ]; then
-    mv -v "${CONF_DIR}/${name}" "${CONF_DIR}/${name}.bak"
-  fi
+	name=$(basename "$item")
+	if [ -d "${CONF_DIR}/${name}" ]; then
+		mv -v "${CONF_DIR}/${name}" "${CONF_DIR}/${name}.bak"
+	fi
 
-  cp -r "${item}" "${CONF_DIR}"
+	cp -r "${item}" "${CONF_DIR}"
 done
 
 # Installing dracula gtk theme and icons
@@ -149,44 +149,44 @@ done
 
 read -p "Change shell to zsh? [y/N] " shell_choice
 if [[ "$shell_choice" =~ ^[Yy]$ ]]; then
-  cp .zshrc "$HOME"
-  chsh -s "$(which zsh)"
+	cp .zshrc "$HOME"
+	chsh -s "$(which zsh)"
 fi
 
 # TPM for tmux
 TPM_DIR="$HOME/.config/tmux/plugins/tpm"
 if [ ! -d "$TPM_DIR" ]; then
-  git clone https://github.com/tmux-plugins/tpm $HOME/.config/tmux/plugins/tpm
+	git clone https://github.com/tmux-plugins/tpm $HOME/.config/tmux/plugins/tpm
 fi
 
 install_nvim_config() {
-  NVIM_DIR="$HOME/.config/nvim"
-  TMP_NVIM_DIR=$(mktemp -d -t neovim-config-XXXXXX)
+	NVIM_DIR="$HOME/.config/nvim"
+	TMP_NVIM_DIR=$(mktemp -d -t neovim-config-XXXXXX)
 
-  if [ -d "${NVIM_DIR}" ]; then
-    mv "${NVIM_DIR}" "${NVIM_DIR}".bak
-  fi
+	if [ -d "${NVIM_DIR}" ]; then
+		mv "${NVIM_DIR}" "${NVIM_DIR}".bak
+	fi
 
-  git clone https://github.com/januarpancaran/neovim-config.git "${TMP_NVIM_DIR}"
-  mkdir -p "${NVIM_DIR}"
-  cp -r "${TMP_NVIM_DIR}"/{lua,init.lua} "$NVIM_DIR"
+	git clone https://github.com/januarpancaran/neovim-config.git "${TMP_NVIM_DIR}"
+	mkdir -p "${NVIM_DIR}"
+	cp -r "${TMP_NVIM_DIR}"/{lua,init.lua} "$NVIM_DIR"
 
-  rm -rf "${TMP_NVIM_DIR}"
+	rm -rf "${TMP_NVIM_DIR}"
 }
 
 read -p "Install my neovim config? [y/N] " nvim_choice
 if [[ "$nvim_choice" =~ ^[Yy]$ ]]; then
-  install_nvim_config
+	install_nvim_config
 fi
 
 read -p "Enable autologin? [y/N] " autologin_choice
 if [[ "$autologin_choice" =~ ^[Yy]$ ]]; then
-  USERNAME=$(whoami)
-  AUTOLOGIN_DIR="/etc/systemd/system/getty@tty1.service.d"
-  "${SUDO_CMD}" mkdir -p "${AUTOLOGIN_DIR}"
+	USERNAME=$(whoami)
+	AUTOLOGIN_DIR="/etc/systemd/system/getty@tty1.service.d"
+	"${SUDO_CMD}" mkdir -p "${AUTOLOGIN_DIR}"
 
-  AUTOLOGIN_CONF="${AUTOLOGIN_DIR}/autologin.conf"
-  "${SUDO_CMD}" tee "${AUTOLOGIN_CONF}" >/dev/null <<EOF
+	AUTOLOGIN_CONF="${AUTOLOGIN_DIR}/autologin.conf"
+	"${SUDO_CMD}" tee "${AUTOLOGIN_CONF}" >/dev/null <<EOF
 [Service]
 ExecStart=
 ExecStart=-/usr/bin/agetty --noclear --autologin ${USERNAME} %I \$TERM
@@ -195,9 +195,9 @@ fi
 
 read -p "Add zlogin to autologin niri? [y/N] " zlogin_choice
 if [[ "$zlogin_choice" =~ ^[Yy]$ ]]; then
-  tee "$HOME/.zlogin" >/dev/null <<EOF
-if [ -z "\$WAYLAND_DISPLAY" ] && [ "\$XDG_VTNR" -eq 1 ] && [ \$(tty) = "/dev/tty1" ]; then
-  dbus-run-session niri --session
+	tee "$HOME/.zlogin" >/dev/null <<EOF
+if [ -z "\$WAYLAND_DISPLAY" ] && [ "\$XDG_VTNR" -eq 1 ] && [ "\$(tty)" = "/dev/tty1" ]; then
+  exec niri --session
 fi
 EOF
 fi
