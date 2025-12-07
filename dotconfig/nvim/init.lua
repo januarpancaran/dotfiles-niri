c6836413a4bbf68f59bde13c4ba74276369d3e1b
januarpancaran <januar352@gmail.com>
2025-12-07 16:56:07 +0700

    fix: fixing init.lua, removing unnecessary files, and updating lazy-lock

--- dotconfig/nvim/init.lua
+++ dotconfig/nvim/init.lua
@@ -1,12 +1,5 @@
-if vim.g.vscode then
-    require("plugins.comment-nvim")
-    require("plugins.indent-blankline-nvim")
-    require("plugins.markdown-preview-nvim")
-    require("plugins.nvim-treesitter-context")
-    require("plugins.rainbow-delimiters-nvim")
-
-else
-    require("config.lazy")
+if not vim.g.vscode then
+	require("config.lazy")
 end
 
 require("config.options")
