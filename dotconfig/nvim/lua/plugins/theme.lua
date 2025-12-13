5fca8cc3f06a05f2dd857293ac2c9e6dd2597254
januarpancaran <januar352@gmail.com>
2025-12-13 12:58:35 +0700

    chore: changing nvim theme to tokyonight

--- dotconfig/nvim/lua/plugins/theme.lua
+++ dotconfig/nvim/lua/plugins/theme.lua
@@ -1,8 +1,8 @@
 return {
-	"catppuccin/nvim",
+	"folke/tokyonight.nvim",
 	priority = 1000,
 	lazy = false,
 	config = function()
-		vim.cmd.colorscheme("catppuccin-mocha")
+		vim.cmd.colorscheme("tokyonight-night")
 	end,
 }
